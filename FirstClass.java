259cd741a6ca7a1f559d93d98d87faa14f6ba59e
Deven <devendesai007@gmail.com>
2023-02-27 19:43:17 +0530

    Java first commit

---
+++ FirstClass.java
@@ -0,0 +1,6 @@
+class FirstClass {
+    public static void main(String args[]) {
+        System.out.print("Hello world \n");
+        System.out.print("I am deven");
+    }
+}
---
+++ loops.java
@@ -0,0 +1,14 @@
+import java.util.*;
+    
+
+public class loops {
+    
+ public static void main(String[] args) {
+     //i++ is also equal to i = i+1  
+   for(int i = 1; i < 10; i = i + 1)
+
+   System.out.println(i);
+
+   } 
+
+}
